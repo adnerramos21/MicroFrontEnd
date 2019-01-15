4f63feeeecf63cbb0c50790301c81b95be4443af
Adner Ramos <adnerramos21@gmail.com>
2019-01-14 20:16:17 -0600

    Revamped notifications to be inside a panel, rather than a dropdown.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,35 @@ import { MENU_ITEMS } from './side-menu';
 })
 export class AppComponent {
   isOpened = true;
+  isNotificationPanelOpened = false;
   isMenuOptionExpanded = false;
   menuItems = MENU_ITEMS;
 
+  notifications = [
+    {
+      title: 'New Applicant',
+      body: {
+        message: 'James Franco uploaded a resume',
+        timeAgo: '20 min ago'
+      }
+    },
+    {
+      title: 'Resume Update',
+      body: {
+        message: 'Bill Hader added the following skill: Node JS',
+        timeAgo: '30 min ago'
+      }
+    },
+    {
+      title: 'Applicant Update',
+      body: {
+        message: 'Martha Steward is no longer available for the position: Backend Developer',
+        timeAgo: '1 day ago'
+      }
+    },
+  ];
+
   toggleExpand() {
-    console.log('toggle');
     this.isMenuOptionExpanded = !this.isMenuOptionExpanded;
   }
 }
