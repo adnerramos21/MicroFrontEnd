163c0e90dc8d9b0b19482808cf9aa3aeea9b5bbb
Adner Ramos <adnerramos21@gmail.com>
2019-03-06 19:08:52 -0600

    Moved server to root. Added express and request on main app.

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const request = require('request');
+var cors = require('cors');
+
+
+const app = express(express.static('./dist/main-app'));
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello express');
+});
+
+app.get('/listofapplicants', (req, res) => {
+    const url = 'https://notka-81395.firebaseio.com/applicants.json';
+
+    request(url, function (error, response, body) {
+        res.send(JSON.parse(body));
+    });
+
+});
+
+app.get('/infobox', (req, res) => {
+    const url = 'https://notka-81395.firebaseio.com/dashboard/infobox.json';
+
+    request(url, function (error, response, body) {
+        res.send(JSON.parse(body));
+    });
+});
+
+app.get('/topapplicants', (req, res) => {
+    const url = 'https://notka-81395.firebaseio.com/dashboard/topApplicants.json';
+
+    request(url, function (error, response, body) {
+        res.send(JSON.parse(body));
+    });
+});
+
+app.get('/notificationpanel', (req, res) => {
+    // Coming soon
+});
+
+
+app.listen(port, () => {
+    console.log('server is up on port ' + port);
+});
+
