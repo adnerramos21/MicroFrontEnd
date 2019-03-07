e3e86dffb3ee0f16b4effc3bea265579f1846eb6
Adner Ramos <adnerramos21@gmail.com>
2019-03-06 18:52:27 -0600

    Test deploying to Heroku

--- web-server/app.js
+++ web-server/app.js
@@ -3,7 +3,7 @@ const request = require('request');
 var cors = require('cors');
 
 
-const app = express();
+const app = express(express.static('./../dist/main-app'));
 const port = process.env.PORT || 3000;
 
 app.use(cors());
