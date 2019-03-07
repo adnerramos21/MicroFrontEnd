31fdadf606a1a6c948725b77bb01f18c8ad8d7db
Adner Ramos <adnerramos21@gmail.com>
2019-03-06 20:11:05 -0600

    adjustment to endpoint

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/main-app/index.html'));
     // res.send('Hello express');
 });
